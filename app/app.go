a3f2494f076cd31d3e11adbd7251bae10b9734b4
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-08-09 07:30:31 +0200

    Changed the Bech32 addresses prefix to did:com:

--- app/app.go
+++ app/app.go
@@ -45,7 +45,7 @@ const (
 	DefaultBondDenom = "ucommercio"
 
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
-	Bech32MainPrefix = "did:comnet:"
+	Bech32MainPrefix = "did:com:"
 
 	// PrefixValidator is the prefix for validator keys
 	PrefixValidator = "val"
@@ -59,15 +59,15 @@ const (
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
 	Bech32PrefixAccAddr = Bech32MainPrefix
 	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
-	Bech32PrefixAccPub = "comnet" + PrefixPublic
+	Bech32PrefixAccPub = Bech32MainPrefix + PrefixPublic
 	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
-	Bech32PrefixValAddr = "comnet" + PrefixValidator + PrefixOperator
+	Bech32PrefixValAddr = Bech32MainPrefix + PrefixValidator + PrefixOperator
 	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
-	Bech32PrefixValPub = "comnet" + PrefixValidator + PrefixOperator + PrefixPublic
+	Bech32PrefixValPub = Bech32MainPrefix + PrefixValidator + PrefixOperator + PrefixPublic
 	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
-	Bech32PrefixConsAddr = "comnet" + PrefixValidator + PrefixConsensus
+	Bech32PrefixConsAddr = Bech32MainPrefix + PrefixValidator + PrefixConsensus
 	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
-	Bech32PrefixConsPub = "comnet" + PrefixValidator + PrefixConsensus + PrefixPublic
+	Bech32PrefixConsPub = Bech32MainPrefix + PrefixValidator + PrefixConsensus + PrefixPublic
 )
 
 // default home directories for expected binaries
