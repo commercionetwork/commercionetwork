e8f3ef83cbae976d6af13aafecdac06b2499cfd6
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-08-26 09:53:41 +0200

    Renamed commercioid package to id and commerciodocs to docs

--- x/id/client/rest/query.go
+++ x/id/client/rest/query.go
@@ -2,10 +2,11 @@ package rest
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) {
