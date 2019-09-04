5abc73855ad8da4b8bdd36d767fee88970ef245b
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-09-04 09:33:18 +0200

    Added missing type doc

--- x/id/internal/types/identity.go
+++ x/id/internal/types/identity.go
@@ -4,6 +4,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// Identity contains the data of an identity that can be specified inside the genesis and are
+// exported during the current state exportation.
 type Identity struct {
 	Owner       sdk.AccAddress `json:"owner"`
 	DidDocument string         `json:"did_document"`
