882afe42b50d7a1ef0642a641df59ed56f6a2085
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-09-18 14:34:59 +0200

    Fixed Vuepress config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: "Commercio.network Documentation",
     description: "Documentation for the Commercio.network blockchain.",
     ga: "UA-51029217-2",
-    base: "/Commercionetwork/",
+    base: "/commercionetwork/",
     markdown: {
         lineNumbers: true
     },
