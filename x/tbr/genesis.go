268b7a06ea43fc85c32dab10ebcdd202df2324ad
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-10-23 10:55:18 +0200

    Fixed the TBR genesis exportation

--- x/tbr/genesis.go
+++ x/tbr/genesis.go
@@ -2,6 +2,7 @@ package tbr
 
 import (
 	"errors"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -19,35 +20,41 @@ func DefaultGenesisState() GenesisState {
 
 // InitGenesis sets the initial Block Reward Pool amount for genesis.
 func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) {
-	if data.PoolAmount != nil {
-		keeper.SetTotalRewardPool(ctx, data.PoolAmount)
-		keeper.SetYearlyRewardPool(ctx, data.PoolAmount.QuoDec(sdk.NewDec(5)))
+	// Set the reward pool - Should never be nil as its validated inside the ValidateGenesis method
+	keeper.SetTotalRewardPool(ctx, data.PoolAmount)
+
+	// Default yearly reward pool amount
+	if data.YearlyPoolAmount == nil {
+		data.YearlyPoolAmount = data.PoolAmount.QuoDec(sdk.NewDec(5))
 	}
 
-	if data.YearlyPoolAmount != nil {
-		keeper.SetYearlyRewardPool(ctx, data.YearlyPoolAmount)
-	}
-
+	// Set the yearly reward pool and year number
+	keeper.SetYearlyRewardPool(ctx, data.YearlyPoolAmount)
 	keeper.SetYearNumber(ctx, data.YearNumber)
 }
 
 // ExportGenesis returns a GenesisState for a given context and keeper.
 func ExportGenesis(ctx sdk.Context, keeper Keeper) GenesisState {
 	return GenesisState{
-		PoolAmount: keeper.GetTotalRewardPool(ctx),
-		YearNumber: keeper.GetYearNumber(ctx),
+		PoolAmount:       keeper.GetTotalRewardPool(ctx),
+		YearlyPoolAmount: keeper.GetYearlyRewardPool(ctx),
+		YearNumber:       keeper.GetYearNumber(ctx),
 	}
 }
 
 // ValidateGenesis performs basic validation of genesis data returning an
 // error for any failed validation criteria.
 func ValidateGenesis(data GenesisState) error {
-	if data.PoolAmount.Empty() {
+	if data.PoolAmount == nil || data.PoolAmount.Empty() {
 		return errors.New("transaction block reward pool cannot be empty")
 	}
 
-	if data.PoolAmount.IsAnyNegative() {
-		return errors.New("transaction block reward pool cannot contain negative funds")
+	if !data.PoolAmount.IsValid() {
+		return errors.New(fmt.Sprintf("invalid transaction block reward pool: %s", data.PoolAmount.String()))
+	}
+
+	if !data.YearlyPoolAmount.IsValid() {
+		return errors.New(fmt.Sprintf("invalid yearly transaction block reward pool: %s", data.YearlyPoolAmount.String()))
 	}
 
 	return nil
