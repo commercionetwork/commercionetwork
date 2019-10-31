028d99d7e32eff880d01d3b68a3701f8ca4e4888
Gianguido Sorà <me@gsora.xyz>
2019-10-31 08:59:22 +0000

    Validate UUIDs using go.uuid
    Test included to verify the correctness of this implementation.

--- x/docs/internal/types/document_test.go
+++ x/docs/internal/types/document_test.go
@@ -19,3 +19,37 @@ func TestDocument_Equals_NilValues(t *testing.T) {
 	}
 	assert.True(t, document.Equals(document))
 }
+
+func Test_validateUUID(t *testing.T) {
+	tests := []struct {
+		name    string
+		UUID    string
+		badUUID bool
+	}{
+		{
+			"empty string",
+			"",
+			true,
+		},
+		{
+			"a well-formed UUID",
+			"6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+			false,
+		},
+		{
+			"a seemingly well-formed UUID, with the last character removed",
+			"6ba7b810-9dad-11d1-80b4-00c04fd430c",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			val := validateUUID(tt.UUID)
+			if tt.badUUID {
+				assert.False(t, val, "got true")
+			} else {
+				assert.True(t, val, "got false")
+			}
+		})
+	}
+}
