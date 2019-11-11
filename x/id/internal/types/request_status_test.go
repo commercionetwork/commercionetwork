5ea828a84783535656a1ed0b01d4bb323a5a5577
Gianguido Sorà <me@gsora.xyz>
2019-11-11 17:30:24 +0100

    request status test to gotests style

--- x/id/internal/types/request_status_test.go
+++ x/id/internal/types/request_status_test.go
@@ -4,15 +4,40 @@ import (
 	"testing"
 
 	"github.com/commercionetwork/commercionetwork/x/id/internal/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRequestStatus_Validate(t *testing.T) {
-
-	err := types.NewRequestStatus("invalid", "message").Validate()
-	assert.Error(t, err)
-	assert.Contains(t, err.Result().Log, "Invalid status type: invalid")
-
-	assert.NoError(t, types.NewRequestStatus("rejected", "").Validate())
-	assert.NoError(t, types.NewRequestStatus("canceled", "").Validate())
+	tests := []struct {
+		name    string
+		rq      types.RequestStatus
+		wantErr sdk.Error
+	}{
+		{
+			"invalid status type",
+			types.NewRequestStatus("invalid", "message"),
+			sdk.ErrUnknownRequest("Invalid status type: invalid"),
+		},
+		{
+			"\"rejected\" type",
+			types.NewRequestStatus("rejected", ""),
+			nil,
+		},
+		{
+			"\"canceled\" type",
+			types.NewRequestStatus("canceled", ""),
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantErr != nil {
+				assert.EqualError(t, tt.rq.Validate(), tt.wantErr.Error())
+			} else {
+				assert.NoError(t, tt.rq.Validate())
+			}
+		})
+	}
 }
