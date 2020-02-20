4cdad470b1a2c76e348b174d24b392032b5fad97
Gianguido Sorà <me@gsora.xyz>
2020-02-20 17:18:57 +0100

    initialize prices when importing genesis
    removed zero height checks

--- x/commerciomint/internal/keeper/invariants.go
+++ x/commerciomint/internal/keeper/invariants.go
@@ -24,11 +24,6 @@ func CdpsForExistingPrice(k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
 		cdps := k.GetCdps(ctx)
 
-		if ctx.BlockHeight() == 0 {
-			// ignore this invariant when blockHeight is zero
-			return "", false
-		}
-
 		for _, cdp := range cdps {
 			for _, depositAmount := range cdp.DepositedAmount {
 				price, ok := k.priceFeedKeeper.GetCurrentPrice(ctx, depositAmount.Denom)
@@ -68,16 +63,14 @@ func LiquidityPoolAmountEqualsCdps(k Keeper) sdk.Invariant {
 
 		pool := k.GetLiquidityPoolAmount(ctx)
 
-		if ctx.BlockHeight() != 0 {
-			if pool.IsZero() && len(cdps) > 0 {
-				return sdk.FormatInvariant(
-					types.ModuleName,
-					cdpsForExistingPrice,
-					fmt.Sprintf(
-						"cdps opened and liquidity pool is empty",
-					),
-				), true
-			}
+		if pool.IsZero() && len(cdps) > 0 {
+			return sdk.FormatInvariant(
+				types.ModuleName,
+				cdpsForExistingPrice,
+				fmt.Sprintf(
+					"cdps opened and liquidity pool is empty",
+				),
+			), true
 		}
 
 		for name, sum := range sums {
