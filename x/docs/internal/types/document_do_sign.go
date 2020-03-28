a69dcf2e936d86f31bb0868a23b900c4335c08af
rgrcoding <redgreenrefactorbcn@gmail.com>
2020-03-28 13:17:30 +0100

    add sdn fields

--- x/docs/internal/types/document_do_sign.go
+++ x/docs/internal/types/document_do_sign.go
@@ -1,8 +1,18 @@
 package types
 
 type DocumentDoSign struct {
-	StorageUri         string `json:"storage_uri"`
-	SignerInstance     string `json:"signer_instance"`
-	VcrId              string `json:"vcr_id"`
-	CertificateProfile string `json:"certificate_profile"`
+	StorageUri         string  `json:"storage_uri"`
+	SignerInstance     string  `json:"signer_instance"`
+	SdnData            SdnData `json:"sdn_data"`
+	VcrId              string  `json:"vcr_id"`
+	CertificateProfile string  `json:"certificate_profile"`
+}
+
+type SdnData struct {
+	FirstName    string
+	LastName     string
+	Tin          string
+	Email        string
+	Organization string
+	Country      string
 }
