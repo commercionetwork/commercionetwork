f8d1671876940570743786d29c28eaf6af518a62
rgrcoding <redgreenrefactorbcn@gmail.com>
2020-04-05 17:23:27 +0200

    remove comment

--- x/commerciomint/keeper/keeper.go
+++ x/commerciomint/keeper/keeper.go
@@ -108,7 +108,6 @@ func (k Keeper) OpenCdp(ctx sdk.Context, depositor sdk.AccAddress, deposit sdk.C
 		return sdkErr.Wrap(sdkErr.ErrInvalidCoins, fmt.Sprintf("Invalid deposit amount: %s", deposit))
 	}
 
-	// Check if all the tokens inside the deposit amount have a price and calculate the total fiat value of them
 	fiatValue, err := k.calculateFiatValue(ctx, deposit)
 	if err != nil {
 		return err
@@ -231,7 +230,6 @@ func (k Keeper) AutoLiquidateCdps(ctx sdk.Context) {
 }
 
 func (k Keeper) calculateFiatValue(ctx sdk.Context, deposit sdk.Coin) (sdk.Dec, error) {
-	// Check if all the tokens inside the deposit amount have a price and calculate the total fiat value of them
 	fiatValue := sdk.ZeroDec()
 	assetPrice, found := k.priceFeedKeeper.GetCurrentPrice(ctx, deposit.Denom)
 	if !found {
