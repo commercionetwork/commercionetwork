f9b8cdc85ff0f8e5b18473a32f7a7b596626f50d
rgrcoding <redgreenrefactorbcn@gmail.com>
2020-04-06 00:05:22 +0200

    add flag

--- x/id/client/cli/tx.go
+++ x/id/client/cli/tx.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	"github.com/spf13/cobra"
 
 	"github.com/commercionetwork/commercionetwork/x/id/types"
@@ -31,7 +33,20 @@ func getSetIdentityCommand(cdc *codec.Codec) *cobra.Command {
 		Use:   "setidentity",
 		Short: "sets the identity",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
+			msg := types.NewMsgSetIdentity(types.DidDocument{
+				Context: "",
+				ID:      nil,
+				PubKeys: nil,
+				Proof:   types.Proof{},
+				Service: nil,
+			})
+
+			err := msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			return utils.CompleteAndBroadcastTxCLI(txBldr, cliCtx, []sdk.Msg{msg})
 		},
 	}
 
