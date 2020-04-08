5c2a6bc37e8608fd68896c9161383ed8236d9efc
rgrcoding <redgreenrefactorbcn@gmail.com>
2020-04-08 14:09:33 +0200

    change testdata

--- x/id/types/priv_key_test.go
+++ x/id/types/priv_key_test.go
@@ -2,18 +2,19 @@ package types
 
 import (
 	"io/ioutil"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetPemPublicKeyFromPemPrivKeyFile(t *testing.T) {
-	privKey, err := LoadRSAPrivKeyFromDisk("testfiles/priv_key.pem")
+	privKey, err := LoadRSAPrivKeyFromDisk(path.Join("testdata", "priv_key.pem"))
 	require.NoError(t, err)
 
 	pemPubKey := PublicKeyToPemString(&privKey.PublicKey)
 
-	expectePemPubKey, err := ioutil.ReadFile("testfiles/pub_key.pem")
+	expectePemPubKey, err := ioutil.ReadFile(path.Join("testdata", "pub_key.pem"))
 	require.NoError(t, err)
 
 	require.Equal(t, string(expectePemPubKey), pemPubKey)
