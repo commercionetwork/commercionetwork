a35d45defb66909349c247421d08740a76b7defc
Gianguido Sorà <me@gsora.xyz>
2020-07-09 16:06:42 +0200

    update x/docs SetupTestInput to compile

--- x/docs/internal/keeper/test_commons.go
+++ x/docs/internal/keeper/test_commons.go
@@ -1,18 +1,55 @@
 package keeper
 
 import (
+	"sync"
+
 	ctypes "github.com/commercionetwork/commercionetwork/x/common/types"
 	"github.com/commercionetwork/commercionetwork/x/docs/internal/types"
 	"github.com/commercionetwork/commercionetwork/x/government"
+	idkeeper "github.com/commercionetwork/commercionetwork/x/id/keeper"
+	idtypes "github.com/commercionetwork/commercionetwork/x/id/types"
+	"github.com/commercionetwork/commercionetwork/x/memberships"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/cosmos/cosmos-sdk/x/supply"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/libs/log"
 	db "github.com/tendermint/tm-db"
+)
+
+var (
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32MainPrefix = "did:com:"
+
+	// PrefixValidator is the prefix for validator keys
+	PrefixValidator = "val"
+	// PrefixConsensus is the prefix for consensus keys
+	PrefixConsensus = "cons"
+	// PrefixPublic is the prefix for public keys
+	PrefixPublic = "pub"
+	// PrefixOperator is the prefix for operator keys
+	PrefixOperator = "oper"
+
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32PrefixAccAddr = Bech32MainPrefix
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = Bech32MainPrefix + PrefixPublic
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = Bech32MainPrefix + PrefixValidator + PrefixOperator
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = Bech32MainPrefix + PrefixValidator + PrefixOperator + PrefixPublic
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = Bech32MainPrefix + PrefixValidator + PrefixConsensus
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = Bech32MainPrefix + PrefixValidator + PrefixConsensus + PrefixPublic
+
+	configSealOnce sync.Once
 )
 
 //This function create an environment to test modules
@@ -21,16 +58,27 @@ func SetupTestInput() (cdc *codec.Codec, ctx sdk.Context, keeper Keeper) {
 	memDB := db.NewMemDB()
 	cdc = testCodec()
 	authKey := sdk.NewKVStoreKey("authCapKey")
+	supKey := sdk.NewKVStoreKey(supply.StoreKey)
+	idsk := sdk.NewKVStoreKey(idtypes.StoreKey)
 	ibcKey := sdk.NewKVStoreKey("ibcCapKey")
 	fckCapKey := sdk.NewKVStoreKey("fckCapKey")
 	keyParams := sdk.NewKVStoreKey(params.StoreKey)
 	tkeyParams := sdk.NewTransientStoreKey(params.TStoreKey)
+
+	configSealOnce.Do(func() {
+		config := sdk.GetConfig()
+		config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+		config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+		config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+		config.Seal()
+	})
 
 	// Store keys
 	keyDocs := sdk.NewKVStoreKey("docs")
 	keyGovernment := sdk.NewKVStoreKey("government")
 
 	ms := store.NewCommitMultiStore(memDB)
+	ms.MountStoreWithDB(supKey, sdk.StoreTypeIAVL, memDB)
 	ms.MountStoreWithDB(ibcKey, sdk.StoreTypeIAVL, memDB)
 	ms.MountStoreWithDB(authKey, sdk.StoreTypeIAVL, memDB)
 	ms.MountStoreWithDB(fckCapKey, sdk.StoreTypeIAVL, memDB)
@@ -42,8 +90,37 @@ func SetupTestInput() (cdc *codec.Codec, ctx sdk.Context, keeper Keeper) {
 
 	ctx = sdk.NewContext(ms, abci.Header{ChainID: "test-chain-id"}, false, log.NewNopLogger())
 
+	pk := params.NewKeeper(cdc, keyParams, tkeyParams)
+	ak := auth.NewAccountKeeper(cdc, authKey, pk.Subspace(auth.DefaultParamspace), auth.ProtoBaseAccount)
+	bk := bank.NewBaseKeeper(ak, pk.Subspace(bank.DefaultParamspace), map[string]bool{
+		types.ModuleName: false,
+	})
+	maccPerms := map[string][]string{
+		types.ModuleName:       nil,
+		memberships.ModuleName: {supply.Burner},
+		idtypes.ModuleName:     nil,
+	}
+	sk := supply.NewKeeper(cdc, supKey, ak, bk, maccPerms)
 	govk := government.NewKeeper(cdc, keyGovernment)
-	dck := NewKeeper(keyDocs, govk, cdc)
+
+	testGovernment, err := sdk.AccAddressFromBech32("did:com:12p24st9asf394jv04e8sxrl9c384jjqwejv0gf")
+	if err != nil {
+		panic(err)
+	}
+
+	err = govk.SetTumblerAddress(ctx, testGovernment)
+	if err != nil {
+		panic(err)
+	}
+	idk := idkeeper.NewKeeper(cdc, idsk, ak, sk)
+
+	// Set initial supply
+	sk.SetSupply(ctx, supply.NewSupply(sdk.NewCoins(sdk.NewInt64Coin("ucommercio", 1))))
+
+	memk := sdk.NewKVStoreKey(memberships.StoreKey)
+	memkeeper := memberships.NewKeeper(cdc, memk, sk, govk, ak)
+
+	dck := NewKeeper(keyDocs, govk, bk, idk, memkeeper, cdc)
 
 	return cdc, ctx, dck
 }
