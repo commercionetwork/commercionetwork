fdb63d40edbd67d9532409462ed20ae4c93df2a0
fabtagliaferro <fabtagliaferro@gmail.com>
2020-11-12 16:11:35 +0100

    register swagger endpoint and add general swagger declarative comments

---
+++ x/swagger.go
@@ -0,0 +1,10 @@
+//go:generate go run github.com/swaggo/swag/cmd/swag init -g swagger.go -o ../swagger
+
+package x
+
+// @title CommercioNetwork API
+// @description Swagger API for CommercioNetwork
+// @contact.name Gianguido Sorà
+// @contact.email me+work@gsora.xyz
+
+// @BasePath /
