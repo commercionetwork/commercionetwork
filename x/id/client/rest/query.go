23e242cc8fde5036e8ce4ae04c5b458d1716b9a5
fabtagliaferro <fabtagliaferro@gmail.com>
2020-11-19 18:49:20 +0100

    complete documentation for id module

--- x/id/client/rest/query.go
+++ x/id/client/rest/query.go
@@ -43,6 +43,15 @@ func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, querierRoute 
 		Methods("GET")
 }
 
+// @Summary Get a user Did Document
+// @Description This endpoint returns a user Did Document, along with the height at which the resource was queried at
+// @ID id_resolveIdentityHandler
+// @Produce json
+// @Param did path string true "Address of the user for which to read the Did Document"
+// @Success 200 {object} x.JSONResult{result=keeper.ResolveIdentityResponse}
+// @Failure 404
+// @Router /identities/{did} [get]
+// @Tags x/id
 func resolveIdentityHandler(cliCtx context.CLIContext, querierRoute string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -64,7 +73,7 @@ func resolveIdentityHandler(cliCtx context.CLIContext, querierRoute string) http
 // @ID id_resolvePowerUpRequest
 // @Produce json
 // @Param id path string true "Request id"
-// @Success 200 {object} rest.resolvePowerUpRequestResponse
+// @Success 200 {object} x.JSONResult{result=types.DidPowerUpRequest}
 // @Failure 404
 // @Router /powerUpRequest/{id} [get]
 // @Tags x/id
@@ -84,13 +93,14 @@ func resolvePowerUpRequestHandler(cliCtx context.CLIContext, querierRoute string
 	}
 }
 
-type resolvePowerUpRequestResponse struct {
-	Height string                               `json:"height" example:"1234"`
-	Result resolveRejectedPowerUpRequestsResult `json:"result"`
-}
-
-var _ resolvePowerUpRequestResponse
-
+// @Summary Get the user Did power up approved requests
+// @Description This endpoint returns the user Did power up approved requests, along with the height at which the resource was queried at
+// @ID id_resolveApprovedPowerUpRequests
+// @Produce json
+// @Success 200 {object} x.JSONResult{result=[]types.DidPowerUpRequest}
+// @Failure 404
+// @Router /approvedPowerUpRequests [get]
+// @Tags x/id
 func resolveApprovedPowerUpRequests(cliCtx context.CLIContext, querierRoute string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		route := fmt.Sprintf("custom/%s/%s", querierRoute, types.QueryGetApprovedPowerUpRequest)
@@ -104,11 +114,11 @@ func resolveApprovedPowerUpRequests(cliCtx context.CLIContext, querierRoute stri
 	}
 }
 
-// @Summary Get the user Did power up rejected requests
+// @Summary Get the user Did power up rejected power up requests
 // @Description This endpoint returns the user Did power up rejected requests, along with the height at which the resource was queried at
 // @ID id_resolveRejectedPowerUpRequests
 // @Produce json
-// @Success 200 {object} rest.resolveRejectedPowerUpRequestsResponse
+// @Success 200 {object} x.JSONResult{result=[]types.DidPowerUpRequest}
 // @Failure 404
 // @Router /rejectedPowerUpRequests [get]
 // @Tags x/id
@@ -125,32 +135,14 @@ func resolveRejectedPowerUpRequests(cliCtx context.CLIContext, querierRoute stri
 	}
 }
 
-type resolveRejectedPowerUpRequestsResponse struct {
-	Height string                                 `json:"height" example:"1234"`
-	Result []resolveRejectedPowerUpRequestsResult `json:"result"`
-}
-
-var _ resolveRejectedPowerUpRequestsResponse
-
-type resolveRejectedPowerUpRequestsResult struct {
-	Status    resolveRejectedPowerUpRequestsStatus
-	Claimant  string         `json:"claimant" example:"did:com:150jp3tx96frukqg6v870etf02q0cp7em78wu48"`
-	Amount    []amountResult `json:"amount"`
-	Proof     string         `json:"proof" example:"S5hyg4slMxm9fK8PTNDs8tHmQcBfWXG0vqrNHLXY5K1qUz3QwZYjR9nzJoNDJh18aPsXper7rNBbyZPOm5K//x8Bqm2EJkdnHd7woa5eFqpziGaHxqvgPaLGspH47tnVilARTeF23L2NVHWcEWuo9U5cWg52l1lOixOG+DehT3vC9KjLqg0YqBoL2u0LTLqQMON4UUjC8JwzT/RMs30OYGsWuLc9s48RtJCQJZ+yAg3U6jZn3OokGwWWjYxF9tAsMR48KilHsPigsa9WPnaAyCMSJ05hOqjBxWiSHYiH1nAefFqHtNFXhJF3LRUCJ2xnSHxJC5Ndj4HFzUjyK4aiV1mtRlRcsqmXU80HEk7IzI74HYpW74F8LzXNsh8Pbl7HXoIzEiOHB5XStFnrxkIL3sYAJGH/pGbX3SxeyfoZhY4ikEyqX3OB7Pat2yHh/63XSPThRVpD7g0gy5N2aKBz3vrHCPhe3QQTzWmKlJOcg1FE5ZtSUEHdVQbm1GD9zP6KZDfbekh9+xU0EFczW9JF/we61LTvMF1KoxaBpL46O/J6ROEOQsb03hLEMadBKxZ+XaqAHiQWKu6G5YH2opNTGKcvSyNfDInOvAygUOfzLgTCWp7JOU09hWBKW1ya2yJNJMZ6q9giEAlqS/qqYy4gAqZKjt7nF0siOb3Vz6zEaXdhCcqrfnNN6n/kFXWz24yAucW+/EHt+hsygEVUZQ=="`
-	Id        string         `json:"id" example:"d423c645-fd50-4841-8138-192ee8e23dde"`
-	Proof_key string         `json:"proof_key" example:"L0QIWxtHeWeUQhmfWqB2n+MZXFqEYctltilM0j69tBd1drUoUSz/vUkaPadQAdKqtQOD43Py7/JZt5IFyx7iDdphzJEX7bqq+B6nC2DQUeISEiXwtDmJYMp20/N23DY2T7L/Z/dzbxRZDWoUhtr9fRPeJL8NHtPqU9YZw2f1tgMk2t/ZMKtBhYzO5BnF8Crmshjw6b6KA3fK+j7YrmF8fVpVFCdz5jd7cprf5RIqwVjt4w1cYZWeKvGLWeGVX3oiCB67EzXZVUCsD03evr90GDY9qGLfUaWJdBkNjByDotLY0OhrKpcZ+O0IZyZv1+YKx7ZDoPAsEJqpqw4M9bGQRg=="`
-}
-
-type amountResult struct {
-	Denom  string `json:"denom" example:"ucommercio"`
-	Amount int    `json:"amount" example:"100000000000000"`
-}
-
-type resolveRejectedPowerUpRequestsStatus struct {
-	Type_   string `json:"type" example:"rejected"`
-	Message string `json:"message" example:"insufficient fund"`
-}
-
+// @Summary Get the user Did power up pending requests
+// @Description This endpoint returns the user Did power up pending requests, along with the height at which the resource was queried at
+// @ID id_resolvePendingPowerUpRequests
+// @Produce json
+// @Success 200 {object} x.JSONResult{result=[]types.DidPowerUpRequest}
+// @Failure 404
+// @Router /pendingPowerUpRequests [get]
+// @Tags x/id
 func resolvePendingPowerUpRequests(cliCtx context.CLIContext, querierRoute string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		route := fmt.Sprintf("custom/%s/%s", querierRoute, types.QueryGetPendingPowerUpRequest)
