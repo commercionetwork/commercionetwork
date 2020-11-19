6f3b6cabac23d5106e94305ff406c36e50d805f6
fabtagliaferro <fabtagliaferro@gmail.com>
2020-11-19 18:46:54 +0100

    include cosmos sdk type dependencies on generate

--- x/swagger.go
+++ x/swagger.go
@@ -1,6 +1,10 @@
-//go:generate go run github.com/swaggo/swag/cmd/swag init -g swagger.go -o ../swagger
+//go:generate go run github.com/swaggo/swag/cmd/swag init --parseDependency -g swagger.go -o ../swagger
 
 package x
+
+import (
+	_ "github.com/cosmos/cosmos-sdk/types"
+)
 
 // @title CommercioNetwork API
 // @description Swagger API for CommercioNetwork
@@ -8,3 +12,8 @@ package x
 // @contact.email me+work@gsora.xyz
 
 // @BasePath /
+
+type JSONResult struct {
+	Height string      `json:"height" example:"1234"`
+	Result interface{} `json:"result"`
+}
