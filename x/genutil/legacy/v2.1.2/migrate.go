f1f01a7f74f453a121f40ea3b4ce5b4f3fa57567
Marco Ruaro <marco.ruaro@tradenet.it>
2020-11-23 15:28:53 +0100

    Delete vbr migration

--- x/genutil/legacy/v2.1.2/migrate.go
+++ x/genutil/legacy/v2.1.2/migrate.go
@@ -5,16 +5,11 @@ import (
 	v212memberships "github.com/commercionetwork/commercionetwork/x/commerciokyc/legacy/v2.1.2"
 	commKycTypes "github.com/commercionetwork/commercionetwork/x/commerciokyc/types"
 
-	//v212vbr "github.com/commercionetwork/commercionetwork/x/vbr/legacy/v2.1.2"
-	//vbrTypes "github.com/commercionetwork/commercionetwork/x/vbr/types"
-
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	govern "github.com/commercionetwork/commercionetwork/x/government"
 	governTypes "github.com/commercionetwork/commercionetwork/x/government/types"
-
-	//v038auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v0_38"
 
 	"github.com/cosmos/cosmos-sdk/x/genutil"
 )
@@ -50,17 +45,6 @@ func Migrate(appState genutil.AppMap) genutil.AppMap {
 		)
 	}
 
-	/*
-		if appState[v212vbr.ModuleName] != nil {
-			var genVbr v212vbr.GenesisState
-			v039Codec.MustUnmarshalJSON(appState[v212vbr.ModuleName], &genVbr)
-
-			delete(appState, v212vbr.ModuleName) // delete old key in case the name changed
-			appState[vbrTypes.ModuleName] = v039Codec.MustMarshalJSON(
-				migrateVbr(genVbr),
-			)
-		}*/
-
 	return appState
 }
 
@@ -88,10 +72,3 @@ func migrateMembership(oldMembership v212memberships.Membership, govAddress sdk.
 		MembershipType: oldMembership.MembershipType,
 	}
 }
-
-/*
-migrateVbr(genVbr v212vbr.GenesisState) vbrTypes.GenesisState {
-	return vbrTypes.GenesisState {
-
-	}
-}*/
