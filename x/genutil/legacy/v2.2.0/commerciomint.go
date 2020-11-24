c35c262f196e0e844519705109f991e7b97dd324
Gianguido Sorà <me@gsora.xyz>
2020-11-24 11:37:22 +0100

    make sure to remove ucommercio's from commerciomint account, and send them to government; update staking amounts by removing uccc

--- x/genutil/legacy/v2.2.0/commerciomint.go
+++ x/genutil/legacy/v2.2.0/commerciomint.go
@@ -14,7 +14,7 @@ import (
 	authTypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
-func commercioMintMigrate(appState genutil.AppMap) genutil.AppMap {
+func commercioMintMigrate(appState genutil.AppMap, govAddress sdk.AccAddress) genutil.AppMap {
 	// Instead of initializing our own codec, use the cosmos x/auth one
 	// Because we must trigger x/supply/internal/types init() function,
 	// call NewEmptyModuleAccount.
@@ -24,12 +24,14 @@ func commercioMintMigrate(appState genutil.AppMap) genutil.AppMap {
 	_ = supply.NewEmptyModuleAccount("fu")
 	accountsCdc := authTypes.ModuleCdc
 
+	// 1. remove uccc from every account, and move commerciomint ucommercio's to government address
 	if appState[auth.ModuleName] != nil {
 		var old auth.GenesisState
 		accountsCdc.MustUnmarshalJSON(appState[auth.ModuleName], &old)
 
 		newAccounts := make(authexported.GenesisAccounts, 0, len(old.Accounts))
 
+		var mintUcomm sdk.Coins
 		for _, account := range old.Accounts {
 			coins := account.GetCoins()
 			comAmount := coins.AmountOf("ucommercio")
@@ -39,7 +41,33 @@ func commercioMintMigrate(appState genutil.AppMap) genutil.AppMap {
 					sdk.NewCoin("ucommercio", comAmount),
 				),
 			)
+
+			accIface := interface{}(account)
+
+			if macc, ok := accIface.(*supply.ModuleAccount); ok && mintUcomm == nil {
+				if macc.GetName() == "commerciomint" {
+					mintUcomm = macc.GetCoins()
+
+					// reset commerciomint coins
+					err := account.SetCoins(sdk.NewCoins())
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+
 			newAccounts = append(newAccounts, account)
+		}
+
+		if mintUcomm != nil {
+			for i := 0; i < len(newAccounts); i++ {
+				if newAccounts[i].GetAddress().Equals(govAddress) {
+					newAccounts[i].SetCoins(
+						newAccounts[i].GetCoins().Add(mintUcomm...),
+					)
+					break
+				}
+			}
 		}
 
 		// delete old state
@@ -61,6 +89,21 @@ func commercioMintMigrate(appState genutil.AppMap) genutil.AppMap {
 
 		appState[commerciomintTypes.ModuleName] = mintCdc.MustMarshalJSON(
 			commerciomint.DefaultGenesisState(),
+		)
+	}
+
+	// 3. staking module must only contain ucommercio
+	if appState[supply.ModuleName] != nil {
+		var old supply.GenesisState
+		supply.ModuleCdc.MustUnmarshalJSON(appState[supply.ModuleName], &old)
+
+		commAmount := old.Supply.AmountOf("ucommercio")
+
+		old.Supply = sdk.NewCoins(sdk.NewCoin("ucommercio", commAmount))
+
+		delete(appState, supply.ModuleName)
+		appState[supply.ModuleName] = supply.ModuleCdc.MustMarshalJSON(
+			old,
 		)
 	}
 
