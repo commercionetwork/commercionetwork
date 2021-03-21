c51257a96734eb056ba4cd52a4a049d3881912c8
Marco Ruaro <marco.ruaro@tradenet.it>
2021-03-21 23:31:39 +0100

    Add tests

--- x/vbr/keeper/querier_test.go
+++ x/vbr/keeper/querier_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 
@@ -24,4 +25,28 @@ func TestQuerier_getBlockRewardsPoolFunds(t *testing.T) {
 	expected, _ := codec.MarshalJSONIndent(cdc, &TestBlockRewardsPool)
 
 	require.Equal(t, expected, actual)
+}
+
+func TestQuerier_queryRewardRate(t *testing.T) {
+	_, ctx, k, _, _, _ := SetupTestInput(false)
+	require.NoError(t, k.SetRewardRate(ctx, sdk.NewDec(2)))
+	querier := NewQuerier(k)
+	actualBz, err := querier(ctx, []string{types.QueryRewardRate}, request)
+	require.Nil(t, err)
+
+	var rate sdk.Dec
+	k.cdc.MustUnmarshalJSON(actualBz, &rate)
+	require.Equal(t, sdk.NewDec(2), rate)
+}
+
+func TestQuerier_queryFreezePeriod(t *testing.T) {
+	_, ctx, k, _, _, _ := SetupTestInput(false)
+	require.NoError(t, k.SetAutomaticWithdraw(ctx, true))
+	querier := NewQuerier(k)
+	actualBz, err := querier(ctx, []string{types.QueryAutomaticWithdraw}, request)
+	require.Nil(t, err)
+
+	var automaticWithdraw bool
+	k.cdc.MustUnmarshalJSON(actualBz, &automaticWithdraw)
+	require.Equal(t, true, automaticWithdraw)
 }
