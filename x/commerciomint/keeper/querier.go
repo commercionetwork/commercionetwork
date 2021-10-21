0766652bdf6161c5861d7e3bf848cb711bed3085
SerigneCheikh98 <fallserignecheikh.98@gmail.com>
2021-10-21 10:10:32 +0200

    rest commerciomint

---
+++ x/commerciomint/keeper/querier.go
@@ -0,0 +1,48 @@
+package keeper
+
+import (
+	"fmt"
+
+	sdkErr "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/commercionetwork/commercionetwork/x/commerciomint/types"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+func NewQuerier(k Keeper, legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
+		switch path[0] {
+		case types.QueryGetEtps:
+			return queryGetEtps(ctx, path[1:], k, legacyQuerierCdc)
+		case types.QueryConversionRateRest:
+			return queryConversionRate(ctx, k, legacyQuerierCdc)
+		case types.QueryFreezePeriodRest:
+			return queryFreezePeriod(ctx, k,legacyQuerierCdc)
+		default:
+			return nil, sdkErr.Wrap(sdkErr.ErrUnknownRequest, fmt.Sprintf("Unknown %s query endpoint", types.ModuleName))
+		}
+	}
+}
+
+func queryGetEtps(ctx sdk.Context, path []string, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+	ownerAddr, _ := sdk.AccAddressFromBech32(path[0])
+	etps := k.GetAllPositionsOwnedBy(ctx, ownerAddr)
+	etpsBz, err := codec.MarshalJSONIndent(legacyQuerierCdc, etps)
+	if err != nil {
+		return nil, sdkErr.Wrap(sdkErr.ErrUnknownRequest, "could not marshal result to JSON")
+	}
+
+	return etpsBz, nil
+}
+
+func queryConversionRate(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+	return codec.MarshalJSONIndent(legacyQuerierCdc, k.GetConversionRate(ctx))
+}
+
+func queryFreezePeriod(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+	return codec.MarshalJSONIndent(legacyQuerierCdc, k.GetFreezePeriod(ctx))
+}
