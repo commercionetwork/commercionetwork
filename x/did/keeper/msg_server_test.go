138732066aadcb22ca4cde6679021a4de6b6d016
fabtagliaferro <fabtagliaferro@gmail.com>
2021-12-02 18:44:12 +0100

    removed id module stuff

--- x/did/keeper/msg_server_test.go
+++ x/did/keeper/msg_server_test.go
@@ -12,3 +12,13 @@ func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
 	keeper, ctx := setupKeeper(t)
 	return NewMsgServerImpl(*keeper), sdk.WrapSDKContext(ctx)
 }
+
+// func TestSetIdentityMsgServerCreate(t *testing.T) {
+// 	srv, ctx := setupMsgServer(t)
+// 	creator := "A"
+// 	for i := 0; i < 5; i++ {
+// 		_, err := srv.SetDid(ctx, &types.MsgSetDid{ID: creator})
+// 		require.NoError(t, err)
+// 		//assert.Equal(t, i, int(resp.UUID))
+// 	}
+// }
