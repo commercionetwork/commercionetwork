ed4e4ba74e2b50f6710db4653bd5477ebde338b0
fabtagliaferro <fabtagliaferro@gmail.com>
2021-12-09 14:20:05 +0100

    ID validation with did:com: prefix

--- x/did/types/validation.go
+++ x/did/types/validation.go
@@ -1,17 +1,29 @@
 package types
 
 import (
+	"strings"
+
 	commons "github.com/commercionetwork/commercionetwork/x/common/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+const didKeyPrefix = "did:com:"
+
+func isValidDidCom(did string) error {
+	_, err := sdk.AccAddressFromBech32(strings.TrimPrefix(did, didKeyPrefix))
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid ID address (%s)", did, err)
+	}
+
+	return nil
+}
+
 func (ddo DidDocument) Validate() error {
 
 	// validate ID
-	_, err := sdk.AccAddressFromBech32(ddo.ID)
-	if err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid ID address (%s)", err)
+	if err := isValidDidCom(ddo.ID); err != nil {
+		return err
 	}
 
 	// validate Context
@@ -21,14 +33,14 @@ func (ddo DidDocument) Validate() error {
 
 	// validate VerificationMethod
 	for _, vm := range ddo.VerificationMethod {
-		if err = vm.Validate(); err != nil {
+		if err := vm.Validate(); err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid VerificationMethod %s %e", vm, err)
 		}
 	}
 
 	// validate Service
 	for _, s := range ddo.Service {
-		err = s.Validate()
+		err := s.Validate()
 		if err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "invalid Service %s %e", s, err)
 		}
@@ -36,14 +48,14 @@ func (ddo DidDocument) Validate() error {
 
 	// validate Authentication
 	for _, a := range ddo.Authentication {
-		if err = a.Validate(); err != nil {
+		if err := a.Validate(); err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "invalid Autentication %s %e", a, err)
 		}
 	}
 
 	// validate AssertionMethod
 	for _, am := range ddo.AssertionMethod {
-		err = am.Validate()
+		err := am.Validate()
 		if err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "invalid Autentication %s %e", am, err)
 		}
@@ -51,21 +63,21 @@ func (ddo DidDocument) Validate() error {
 
 	// validate CapabilityDelegation
 	for _, cd := range ddo.CapabilityDelegation {
-		if err = cd.Validate(); err != nil {
+		if err := cd.Validate(); err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "invalid CapabilityDelegation %s %e", cd, err)
 		}
 	}
 
 	// validate CapabilityInvocation
 	for _, ci := range ddo.CapabilityInvocation {
-		if err = ci.Validate(); err != nil {
+		if err := ci.Validate(); err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "invalid CapabilityInvocation %s %e", ci, err)
 		}
 	}
 
 	// validate KeyAgreement
 	for _, ka := range ddo.KeyAgreement {
-		if err = ka.Validate(); err != nil {
+		if err := ka.Validate(); err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "invalid KeyAgreement %s %e", ka, err)
 		}
 	}
