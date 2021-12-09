a22ccae2c311762cefa28f094cffee789a675f5d
fabtagliaferro <fabtagliaferro@gmail.com>
2021-12-09 17:29:07 +0100

    added cryptograpic suite list

---
+++ x/did/types/cryptographic.go
@@ -0,0 +1,22 @@
+package types
+
+var cryptographicSuite = []string{
+	"Ed25519Signature2018",
+	"Ed25519VerificationKey2018",
+	"RsaSignature2018",
+	"RsaVerificationKey2018",
+	"EcdsaSecp256k1Signature2019",
+	"EcdsaSecp256k1VerificationKey2019",
+	"EcdsaSecp256k1RecoverySignature2020",
+	"EcdsaSecp256k1RecoveryMethod2020",
+	"JsonWebSignature2020",
+	"JwsVerificationKey2020",
+	"GpgSignature2020",
+	"GpgVerificationKey2020",
+	"JcsEd25519Signature2020",
+	"JcsEd25519Key2020",
+	"BbsBlsSignature2020",
+	"BbsBlsSignatureProof2020",
+	"Bls12381G1Key2020",
+	"Bls12381G2Key2020",
+}
