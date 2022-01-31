0c437d7d1d1c846f8e557fe126c9fb3edaf182db
Marco Ruaro <marco.ruaro@tradenet.it>
2022-01-31 18:25:30 +0100

    Fix migrate commerciomint

--- x/commerciomint/legacy/v3.0.0/migrate.go
+++ x/commerciomint/legacy/v3.0.0/migrate.go
@@ -1,8 +1,11 @@
 package v3_0_0
 
 import (
+	"time"
+
 	v220commerciomint "github.com/commercionetwork/commercionetwork/x/commerciomint/legacy/v2.2.0"
 	"github.com/commercionetwork/commercionetwork/x/commerciomint/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // Migrate accepts exported genesis state from v2.2.0 and migrates it to v3.0.0
@@ -11,11 +14,15 @@ func Migrate(oldGenState v220commerciomint.GenesisState) *types.GenesisState {
 	var positions []*types.Position
 	for _, oldPosition := range oldGenState.Positions {
 		var position types.Position
+		var timePosition time.Time
+		timePosition = oldPosition.CreatedAt
+		var credits sdk.Coin
+		credits = oldPosition.Credits
 
 		position.Owner = oldPosition.Owner.String()
 		position.Collateral = oldPosition.Collateral.Int64()
-		position.CreatedAt = &oldPosition.CreatedAt
-		position.Credits = &oldPosition.Credits
+		position.CreatedAt = &timePosition
+		position.Credits = &credits
 		position.ExchangeRate = oldPosition.ExchangeRate
 		position.ID = oldPosition.ID
 
