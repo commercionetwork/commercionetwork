f4206a04518202e66812994f1901028fd879505c
SerigneCheikh98 <fallserignecheikh.98@gmail.com>
2022-02-01 11:46:58 +0100

    government migrate reviewed & test added

--- x/government/legacy/v3.0.0/types.go
+++ x/government/legacy/v3.0.0/types.go
@@ -1,14 +1,6 @@
 package v3_0_0
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
 const (
 	ModuleName = "government"
 )
 
-// GenesisState - government genesis state
-type GenesisState struct {
-	GovernmentAddress sdk.AccAddress `json:"government_address"`
-}
