c45299cf0498d03a31e2f33fd468792bfb1fe762
Marco Ruaro <marco.ruaro@tradenet.it>
2022-02-01 11:12:55 +0100

    Marco/fix migrate pointer (#293)
    * Fix commerciomint and commerciokyc migrate
    * Add commerciomint and commerciokyc tests

--- x/commerciomint/legacy/v3.0.0/migrate.go
+++ x/commerciomint/legacy/v3.0.0/migrate.go
@@ -1,38 +1,39 @@
 package v3_0_0
 
 import (
+	"time"
+
 	v220commerciomint "github.com/commercionetwork/commercionetwork/x/commerciomint/legacy/v2.2.0"
 	"github.com/commercionetwork/commercionetwork/x/commerciomint/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // Migrate accepts exported genesis state from v2.2.0 and migrates it to v3.0.0
-func Migrate(oldGenState v220commerciomint.GenesisState) *types.GenesisState {
+func Migrate(v220GenState v220commerciomint.GenesisState) *types.GenesisState {
 
 	var positions []*types.Position
-	for _, oldPosition := range oldGenState.Positions {
-		var position types.Position
+	for _, v220Position := range v220GenState.Positions {
+		var timePosition time.Time
+		timePosition = v220Position.CreatedAt
+		var credits sdk.Coin
+		credits = v220Position.Credits
 
-		position.Owner = oldPosition.Owner.String()
-		position.Collateral = oldPosition.Collateral.Int64()
-		position.CreatedAt = &oldPosition.CreatedAt
-		position.Credits = &oldPosition.Credits
-		position.ExchangeRate = oldPosition.ExchangeRate
-		position.ID = oldPosition.ID
-
-		positions = append(positions, &position)
+		positions = append(positions, &types.Position{
+			Owner:        v220Position.Owner.String(),
+			Collateral:   v220Position.Collateral.Int64(),
+			CreatedAt:    &timePosition,
+			Credits:      &credits,
+			ExchangeRate: v220Position.ExchangeRate,
+			ID:           v220Position.ID,
+		})
 	}
-
-	/*var coins []*sdk.Coin
-	for _, coin := range oldGenState.LiquidityPoolAmount {
-		coins = append(coins, &coin)
-	}*/
 
 	return &types.GenesisState{
 		Positions:  positions,
-		PoolAmount: oldGenState.LiquidityPoolAmount,
+		PoolAmount: v220GenState.LiquidityPoolAmount,
 		Params: types.Params{
-			ConversionRate: oldGenState.CollateralRate,
-			FreezePeriod:   oldGenState.FreezePeriod,
+			ConversionRate: v220GenState.CollateralRate,
+			FreezePeriod:   v220GenState.FreezePeriod,
 		},
 	}
 
