e5dcdeaf0cc6a646d89deaa3981c255d4d3758b3
fabtagliaferro <fabtagliaferro@gmail.com>
2022-02-07 11:07:29 +0100

    improve error descriptions in document validation

--- x/documents/types/document.go
+++ x/documents/types/document.go
@@ -110,25 +110,25 @@ func (doc Document) Validate() error {
 	}
 
 	if !validateUUID(doc.UUID) {
-		return sdkErr.Wrap(sdkErr.ErrUnknownRequest, fmt.Sprintf("Invalid document UUID: %s", doc.UUID))
+		return sdkErr.Wrap(sdkErr.ErrInvalidRequest, fmt.Sprintf("Invalid document UUID: %s", doc.UUID))
 	}
 
 	err := doc.Metadata.Validate()
 	if err != nil {
-		return sdkErr.Wrap(sdkErr.ErrUnknownRequest, err.Error())
+		return sdkErr.Wrap(sdkErr.ErrInvalidRequest, err.Error())
 	}
 
 	if doc.Checksum != nil {
 		err = doc.Checksum.Validate()
 		if err != nil {
-			return sdkErr.Wrap(sdkErr.ErrUnknownRequest, err.Error())
+			return sdkErr.Wrap(sdkErr.ErrInvalidRequest, err.Error())
 		}
 	}
 
 	if doc.EncryptionData != nil {
 		err = doc.EncryptionData.Validate()
 		if err != nil {
-			return sdkErr.Wrap(sdkErr.ErrUnknownRequest, err.Error())
+			return sdkErr.Wrap(sdkErr.ErrInvalidRequest, err.Error())
 		}
 
 		// check that each document recipient have some encrypted data
@@ -157,7 +157,7 @@ func (doc Document) Validate() error {
 
 		// Check that the `encrypted_data' field name is actually present in doc
 		fNotPresent := func(s string) error {
-			return sdkErr.Wrap(sdkErr.ErrUnknownRequest,
+			return sdkErr.Wrap(sdkErr.ErrInvalidRequest,
 				fmt.Sprintf("field \"%s\" not present in document, but marked as encrypted", s),
 			)
 		}
@@ -181,14 +181,14 @@ func (doc Document) Validate() error {
 	if doc.DoSign != nil {
 		if doc.Checksum == nil {
 			return sdkErr.Wrap(
-				sdkErr.ErrUnknownRequest,
+				sdkErr.ErrInvalidRequest,
 				"field \"checksum\" not present in document, but required when using do_sign",
 			)
 		}
 
 		if doc.ContentURI == "" {
 			return sdkErr.Wrap(
-				sdkErr.ErrUnknownRequest,
+				sdkErr.ErrInvalidRequest,
 				"field \"content_uri\" not present in document, but required when using do_sign",
 			)
 		}
@@ -196,7 +196,7 @@ func (doc Document) Validate() error {
 			err := *(doc.DoSign).SdnData.Validate()
 			if err != nil {
 				return sdkErr.Wrap(
-					sdkErr.ErrUnknownRequest,
+					sdkErr.ErrInvalidRequest,
 					err.Error(),
 				)
 			}*/
