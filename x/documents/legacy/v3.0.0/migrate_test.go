eac969f4e92df67fc163d0904b1fd2ff5161d4a9
fabtagliaferro <fabtagliaferro@gmail.com>
2022-02-08 18:08:42 +0100

    using shared test material from types in migrate

--- x/documents/legacy/v3.0.0/migrate_test.go
+++ x/documents/legacy/v3.0.0/migrate_test.go
@@ -10,53 +10,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var sender, _ = sdk.AccAddressFromBech32("cosmos1lwmppctrr6ssnrmuyzu554dzf50apkfvd53jx0")
-var recipient, _ = sdk.AccAddressFromBech32("cosmos1tupew4x3rhh0lpqha9wvzmzxjr4e37mfy3qefm")
+var validDocument = types.ValidDocument
 
-var validDocument = types.Document{
-	UUID:       "d83422c6-6e79-4a99-9767-fcae46dfa371",
-	ContentURI: "https://example.com/document",
-	Metadata: &types.DocumentMetadata{
-		ContentURI: "https://example.com/document/metadata",
-		Schema: &types.DocumentMetadataSchema{
-			URI:     "https://example.com/document/metadata/schema",
-			Version: "1.0.0",
-		},
-	},
-	Checksum: &types.DocumentChecksum{
-		Value:     "93dfcaf3d923ec47edb8580667473987",
-		Algorithm: "md5",
-	},
-	EncryptionData: &types.DocumentEncryptionData{
-		Keys:          []*types.DocumentEncryptionKey{{Recipient: recipient.String(), Value: "6F7468657276616C7565"}},
-		EncryptedData: []string{"content", "content_uri", "metadata.content_uri", "metadata.schema.uri"},
-	},
-	DoSign: &types.DocumentDoSign{
-		StorageURI:     "https://example.com/document/storage",
-		SignerInstance: "SignerInstance",
-		SdnData: types.SdnData{
-			types.SdnDataCommonName,
-			types.SdnDataSurname,
-			types.SdnDataSurname,
-			types.SdnDataGivenName,
-			types.SdnDataOrganization,
-			types.SdnDataCountry,
-		},
-		VcrID:              "VcrID",
-		CertificateProfile: "CertificateProfile",
-	},
-	Sender:     sender.String(),
-	Recipients: []string{recipient.String()},
-}
-
-var validDocumentReceipt = types.DocumentReceipt{
-	UUID:         "9680b9f8-d3a2-49c4-b32e-517632e6e67d",
-	Sender:       sender.String(),
-	Recipient:    recipient.String(),
-	TxHash:       "txHash",
-	DocumentUUID: validDocument.UUID,
-	Proof:        "proof",
-}
+var validDocumentReceipt = types.ValidDocumentReceipt
 
 var anotherValidDocument types.Document
 var anotherDocumentReceipt types.DocumentReceipt
@@ -99,6 +55,16 @@ func init() {
 
 	anotherDocumentReceipt = validDocumentReceipt
 	anotherDocumentReceipt.UUID = "7f4d6197-900a-44af-af22-3a703c568bfe"
+	anotherDocumentReceipt.DocumentUUID = anotherValidDocument.UUID
+
+	sender, err := sdk.AccAddressFromBech32(validDocumentReceipt.Sender)
+	if err != nil {
+		panic(err)
+	}
+	recipient, err := sdk.AccAddressFromBech32(validDocumentReceipt.Recipient)
+	if err != nil {
+		panic(err)
+	}
 
 	v220Document = v220docs.Document{
 		Sender:     sender,
@@ -214,95 +180,95 @@ func TestMigrate(t *testing.T) {
 		args args
 		want *types.GenesisState
 	}{
-		// {
-		// 	name: "empty",
-		// 	args: args{
-		// 		oldGenState: v220docs.GenesisState{
-		// 			Documents:                      []v220docs.Document{},
-		// 			Receipts:                       []v220docs.DocumentReceipt{},
-		// 			SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
-		// 			TrustedMetadataSchemaProposers: []sdk.AccAddress{},
-		// 		},
-		// 	},
-		// 	want: &types.GenesisState{
-		// 		Documents: []*types.Document{},
-		// 		Receipts:  []*types.DocumentReceipt{},
-		// 	},
-		// },
-		// {
-		// 	name: "one document and corresponding receipt",
-		// 	args: args{
-		// 		oldGenState: v220docs.GenesisState{
-		// 			Documents: []v220docs.Document{
-		// 				v220Document,
-		// 			},
-		// 			Receipts: []v220docs.DocumentReceipt{
-		// 				v220DocumentReceipt,
-		// 			},
-		// 			SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
-		// 			TrustedMetadataSchemaProposers: []sdk.AccAddress{},
-		// 		},
-		// 	},
-		// 	want: &types.GenesisState{
-		// 		Documents: []*types.Document{
-		// 			&validDocument,
-		// 		},
-		// 		Receipts: []*types.DocumentReceipt{
-		// 			&validDocumentReceipt,
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name: "one document and corresponding receipt, plus one that is invalid so it is not included",
-		// 	args: args{
-		// 		oldGenState: v220docs.GenesisState{
-		// 			Documents: []v220docs.Document{
-		// 				v220Document,
-		// 				invalidV220Document,
-		// 			},
-		// 			Receipts: []v220docs.DocumentReceipt{
-		// 				v220DocumentReceipt,
-		// 			},
-		// 			SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
-		// 			TrustedMetadataSchemaProposers: []sdk.AccAddress{},
-		// 		},
-		// 	},
-		// 	want: &types.GenesisState{
-		// 		Documents: []*types.Document{
-		// 			&validDocument,
-		// 		},
-		// 		Receipts: []*types.DocumentReceipt{
-		// 			&validDocumentReceipt,
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name: "two document and corresponding receipts",
-		// 	args: args{
-		// 		oldGenState: v220docs.GenesisState{
-		// 			Documents: []v220docs.Document{
-		// 				v220Document,
-		// 				anotherV220Document,
-		// 			},
-		// 			Receipts: []v220docs.DocumentReceipt{
-		// 				v220DocumentReceipt,
-		// 				anotherV220DocumentReceipt,
-		// 			},
-		// 			SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
-		// 			TrustedMetadataSchemaProposers: []sdk.AccAddress{},
-		// 		},
-		// 	},
-		// 	want: &types.GenesisState{
-		// 		Documents: []*types.Document{
-		// 			&validDocument,
-		// 			&anotherValidDocument,
-		// 		},
-		// 		Receipts: []*types.DocumentReceipt{
-		// 			&validDocumentReceipt,
-		// 			&anotherDocumentReceipt,
-		// 		},
-		// 	},
-		// },
+		{
+			name: "empty",
+			args: args{
+				oldGenState: v220docs.GenesisState{
+					Documents:                      []v220docs.Document{},
+					Receipts:                       []v220docs.DocumentReceipt{},
+					SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
+					TrustedMetadataSchemaProposers: []sdk.AccAddress{},
+				},
+			},
+			want: &types.GenesisState{
+				Documents: []*types.Document{},
+				Receipts:  []*types.DocumentReceipt{},
+			},
+		},
+		{
+			name: "one document and corresponding receipt",
+			args: args{
+				oldGenState: v220docs.GenesisState{
+					Documents: []v220docs.Document{
+						v220Document,
+					},
+					Receipts: []v220docs.DocumentReceipt{
+						v220DocumentReceipt,
+					},
+					SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
+					TrustedMetadataSchemaProposers: []sdk.AccAddress{},
+				},
+			},
+			want: &types.GenesisState{
+				Documents: []*types.Document{
+					&validDocument,
+				},
+				Receipts: []*types.DocumentReceipt{
+					&validDocumentReceipt,
+				},
+			},
+		},
+		{
+			name: "one document and corresponding receipt, plus one that is invalid so it is not included",
+			args: args{
+				oldGenState: v220docs.GenesisState{
+					Documents: []v220docs.Document{
+						v220Document,
+						invalidV220Document,
+					},
+					Receipts: []v220docs.DocumentReceipt{
+						v220DocumentReceipt,
+					},
+					SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
+					TrustedMetadataSchemaProposers: []sdk.AccAddress{},
+				},
+			},
+			want: &types.GenesisState{
+				Documents: []*types.Document{
+					&validDocument,
+				},
+				Receipts: []*types.DocumentReceipt{
+					&validDocumentReceipt,
+				},
+			},
+		},
+		{
+			name: "two document and corresponding receipts",
+			args: args{
+				oldGenState: v220docs.GenesisState{
+					Documents: []v220docs.Document{
+						v220Document,
+						anotherV220Document,
+					},
+					Receipts: []v220docs.DocumentReceipt{
+						v220DocumentReceipt,
+						anotherV220DocumentReceipt,
+					},
+					SupportedMetadataSchemes:       []v220docs.MetadataSchema{},
+					TrustedMetadataSchemaProposers: []sdk.AccAddress{},
+				},
+			},
+			want: &types.GenesisState{
+				Documents: []*types.Document{
+					&validDocument,
+					&anotherValidDocument,
+				},
+				Receipts: []*types.DocumentReceipt{
+					&validDocumentReceipt,
+					&anotherDocumentReceipt,
+				},
+			},
+		},
 		{
 			name: "one document and corresponding receipt plus a spurious receipt that is not included",
 			args: args{
