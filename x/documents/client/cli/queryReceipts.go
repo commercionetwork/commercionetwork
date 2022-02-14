37932876f372d2937ce44405a9e26b05c61414eb
fabtagliaferro <fabtagliaferro@gmail.com>
2022-02-14 17:33:06 +0100

    added cli and rest entries for DocumentReceipts

---
+++ x/documents/client/cli/queryReceipts.go
@@ -0,0 +1,120 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/commercionetwork/commercionetwork/x/documents/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/spf13/cobra"
+)
+
+func CmdSentReceipts() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sent-receipts [user-address]",
+		Short: "Get the receipts sent by user",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+			addr, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			params := &types.QueryGetSentDocumentsReceiptsRequest{
+				Address:    addr.String(),
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.SentDocumentsReceipts(context.Background(), params)
+			if err != nil {
+				fmt.Printf("could not get any sent receipt for the given address: \n %s", err)
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdReceivedReceipts() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "received-receipts [user-address]",
+		Short: "Get the receipts received by user",
+
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			addr, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			params := &types.QueryGetReceivedDocumentsReceiptsRequest{
+				Address:    addr.String(),
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.ReceivedDocumentsReceipts(context.Background(), params)
+			if err != nil {
+				fmt.Printf("could not get any received receipt for the given address: \n %s", err)
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdDocumentsReceipts() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "documents-receipts [documentUUID]",
+		Short: "Get the receipts associated with the given document ID",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			params := &types.QueryGetReceivedDocumentsReceiptsRequest{
+				Address:    args[0],
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.ReceivedDocumentsReceipts(context.Background(), params)
+			if err != nil {
+				fmt.Printf("could not get any received receipt for the given document UUID: \n %s", err)
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
--- x/documents/client/rest/query.go
+++ x/documents/client/rest/query.go
@@ -11,28 +11,34 @@ import (
 )
 
 const (
-	addressRestParameterName = "user"
+	addressUserRestParameterName = "user"
+	addressUUIDRestParameterName = "UUID"
 )
 
 func RegisterRoutes(cliCtx client.Context, r *mux.Router) {
 	r.HandleFunc(
-		fmt.Sprintf("/commercionetwork/docs/{%s}/sent", addressRestParameterName),
+		fmt.Sprintf("/commercionetwork/docs/{%s}/sent", addressUserRestParameterName),
 		getSentDocumentsHandler(cliCtx)).
 		Methods("GET")
 
 	r.HandleFunc(
-		fmt.Sprintf("/commercionetwork/docs/{%s}/received", addressRestParameterName),
+		fmt.Sprintf("/commercionetwork/docs/{%s}/received", addressUserRestParameterName),
 		getReceivedDocumentsHandler(cliCtx)).
 		Methods("GET")
 
 	r.HandleFunc(
-		fmt.Sprintf("/commercionetwork/receipts/{%s}/sent", addressRestParameterName),
+		fmt.Sprintf("/commercionetwork/receipts/{%s}/sent", addressUserRestParameterName),
 		getSentDocumentsReceiptsHandler(cliCtx)).
 		Methods("GET")
 
 	r.HandleFunc(
-		fmt.Sprintf("/commercionetwork/receipts/{%s}/received", addressRestParameterName),
+		fmt.Sprintf("/commercionetwork/receipts/{%s}/received", addressUserRestParameterName),
 		getReceivedDocumentsReceiptsHandler(cliCtx)).
+		Methods("GET")
+
+	r.HandleFunc(
+		fmt.Sprintf("/commercionetwork/docs/{%s}/receipts", addressUUIDRestParameterName),
+		getDocumentsReceiptsHandler(cliCtx)).
 		Methods("GET")
 }
 
@@ -43,7 +49,7 @@ func RegisterRoutes(cliCtx client.Context, r *mux.Router) {
 func getSentDocumentsHandler(cliCtx client.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		address := vars[addressRestParameterName]
+		address := vars[addressUserRestParameterName]
 
 		route := fmt.Sprintf("custom/%s/%s/%s", types.QuerierRoute, address, types.QuerySentDocuments)
 		res, _, err := cliCtx.QueryWithData(route, nil)
@@ -59,7 +65,7 @@ func getSentDocumentsHandler(cliCtx client.Context) http.HandlerFunc {
 func getReceivedDocumentsHandler(cliCtx client.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		address := vars[addressRestParameterName]
+		address := vars[addressUserRestParameterName]
 
 		route := fmt.Sprintf("custom/%s/%s/%s", types.QuerierRoute, address, types.QueryReceivedDocuments)
 		res, _, err := cliCtx.QueryWithData(route, nil)
@@ -79,7 +85,7 @@ func getReceivedDocumentsHandler(cliCtx client.Context) http.HandlerFunc {
 func getSentDocumentsReceiptsHandler(cliCtx client.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		address := vars[addressRestParameterName]
+		address := vars[addressUserRestParameterName]
 
 		route := fmt.Sprintf("custom/%s/%s/%s", types.QuerierRoute, address, types.QuerySentReceipts)
 		res, _, err := cliCtx.QueryWithData(route, nil)
@@ -95,7 +101,23 @@ func getSentDocumentsReceiptsHandler(cliCtx client.Context) http.HandlerFunc {
 func getReceivedDocumentsReceiptsHandler(cliCtx client.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		address := vars[addressRestParameterName]
+		address := vars[addressUserRestParameterName]
+
+		route := fmt.Sprintf("custom/%s/%s/%s", types.QuerierRoute, address, types.QueryReceivedReceipts)
+		res, _, err := cliCtx.QueryWithData(route, nil)
+		if err != nil {
+			restTypes.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
+		restTypes.PostProcessResponse(w, cliCtx, res)
+	}
+}
+
+func getDocumentsReceiptsHandler(cliCtx client.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		address := vars[addressUUIDRestParameterName]
 
 		route := fmt.Sprintf("custom/%s/%s/%s", types.QuerierRoute, address, types.QueryReceivedReceipts)
 		res, _, err := cliCtx.QueryWithData(route, nil)
