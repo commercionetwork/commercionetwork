0c5d66824befe2c6bf4d30ba01718b1fd8843b34
fabtagliaferro <fabtagliaferro@gmail.com>
2022-02-24 17:19:28 +0100

    tested params type

---
+++ x/vbr/types/params_test.go
@@ -0,0 +1,104 @@
+package types
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var validEarnRate = sdk.NewDecWithPrec(5, 1)
+var invalidEarnRate = sdk.NewDecWithPrec(-5, 1)
+
+// invalidEarnRate := validEarnRate.
+
+func TestParams_Validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		params  Params
+		wantErr bool
+	}{
+		{
+			name:   "ok",
+			params: DefaultParams(),
+		},
+		{
+			name:    "invalid DistrEpochIdentifier",
+			params:  NewParams("", validEarnRate),
+			wantErr: true,
+		},
+		{
+			name: "invalid EarnRate",
+			params: Params{
+				DistrEpochIdentifier: EpochDay,
+				EarnRate:             invalidEarnRate,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if err := tt.params.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Params.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_validateDistrEpochIdentifier(t *testing.T) {
+	tests := []struct {
+		name                 string
+		distrEpochIdentifier interface{}
+		wantErr              bool
+	}{
+		{
+			name:                 "ok",
+			distrEpochIdentifier: EpochDay,
+		},
+		{
+			name:    "wrong type",
+			wantErr: true,
+		},
+		{
+			name:                 "empty",
+			distrEpochIdentifier: "",
+			wantErr:              true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validateDistrEpochIdentifier(tt.distrEpochIdentifier); (err != nil) != tt.wantErr {
+				t.Errorf("validateDistrEpochIdentifier() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_validateEarnRate(t *testing.T) {
+	tests := []struct {
+		name     string
+		earnRate interface{}
+		wantErr  bool
+	}{
+		{
+			name:     "ok",
+			earnRate: validEarnRate,
+		},
+		{
+			name:    "wrong type",
+			wantErr: true,
+		},
+		{
+			name:     "negative",
+			earnRate: invalidEarnRate,
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validateEarnRate(tt.earnRate); (err != nil) != tt.wantErr {
+				t.Errorf("validateEarnRate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
