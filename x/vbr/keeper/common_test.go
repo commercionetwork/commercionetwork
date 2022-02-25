a6f718a40f9f4dbc8ba39dc9cab8ee0d945c6656
fabtagliaferro <fabtagliaferro@gmail.com>
2022-02-25 14:22:21 +0100

    remove unused test args

--- x/vbr/keeper/common_test.go
+++ x/vbr/keeper/common_test.go
@@ -9,11 +9,9 @@ import (
 )
 
 var (
-	distrAcc      = accountTypes.NewEmptyModuleAccount(types.ModuleName)
-	TestFunder, _ = sdk.AccAddressFromBech32("cosmos1lwmppctrr6ssnrmuyzu554dzf50apkfvd53jx0")
+	distrAcc = accountTypes.NewEmptyModuleAccount(types.ModuleName)
 
 	valAddrVal, _    = sdk.ValAddressFromBech32("cosmosvaloper1tflk30mq5vgqjdly92kkhhq3raev2hnz6eete3")
-	valDelAddr, _    = sdk.AccAddressFromBech32("cosmos1nynns8ex9fq6sjjfj8k79ymkdz4sqth06xexae")
 	PKs              = simapp.CreateTestPubKeys(10)
 	TestValidator, _ = stakingTypes.NewValidator(valAddrVal, PKs[0], stakingTypes.Description{})
 )
