e12324b25a4aebc5740cd61066a2d039c8a67c51
fabtagliaferro <fabtagliaferro@gmail.com>
2022-03-03 13:21:12 +0100

    improving cli test structure, added test for CmdSentDocuments

--- x/documents/client/cli/queryDocument_test.go
+++ x/documents/client/cli/queryDocument_test.go
@@ -3,6 +3,7 @@ package cli_test
 import (
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/client"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	"github.com/stretchr/testify/require"
 
@@ -12,35 +13,52 @@ import (
 	govTypes "github.com/commercionetwork/commercionetwork/x/government/types"
 )
 
-func TestCmdShowDocument(t *testing.T) {
+var governmentGenesisState = govTypes.GenesisState{
+	GovernmentAddress: "cosmos1wze8mn5nsgl9qrgazq6a92fvh7m5e6psjcx2du",
+}
+var documentsGenesisState = types.GenesisState{
+	Documents: []*types.Document{&types.ValidDocument},
+	Receipts: []*types.DocumentReceipt{
+		&types.ValidDocumentReceiptRecipient1,
+		&types.ValidDocumentReceiptRecipient2,
+	},
+}
+
+var ctx client.Context
+
+func TestTx(t *testing.T) {
+
 	cfg := network.DefaultConfig()
 
-	stateGov := govTypes.GenesisState{}
-	require.NoError(t, cfg.Codec.UnmarshalJSON(cfg.GenesisState[govTypes.ModuleName], &stateGov))
-	stateGov.GovernmentAddress = "cosmos1nynns8ex9fq6sjjfj8k79ymkdz4sqth06xexae"
-	bufGov, _ := cfg.Codec.MarshalJSON(&stateGov)
+	bufGov, err := cfg.Codec.MarshalJSON(&governmentGenesisState)
+	require.NoError(t, err)
 	cfg.GenesisState[govTypes.ModuleName] = bufGov
 
-	state := types.GenesisState{
-		Documents: []*types.Document{&types.ValidDocument},
-		Receipts:  []*types.DocumentReceipt{},
-	}
-	buf, err := cfg.Codec.MarshalJSON(&state)
+	buf, err := cfg.Codec.MarshalJSON(&documentsGenesisState)
 	require.NoError(t, err)
 	cfg.GenesisState[types.ModuleName] = buf
 
 	net := network.New(t, cfg)
 	val := net.Validators[0]
-	ctx := val.ClientCtx
+	ctx = val.ClientCtx
+
+	t.Run("CmdShowDocument", testCmdShowDocument)
+	t.Run("CmdSentDocuments", testCmdSentDocuments)
+
+}
+
+func testCmdShowDocument(t *testing.T) {
 
 	for _, tt := range []struct {
-		name    string
-		args    []string
-		wantErr bool
+		name     string
+		args     []string
+		expected *types.Document
+		wantErr  bool
 	}{
 		{
-			name: "ok",
-			args: []string{types.ValidDocument.UUID},
+			name:     "ok",
+			args:     []string{types.ValidDocument.UUID},
+			expected: &types.ValidDocument,
 		},
 		{
 			name:    "document not in store",
@@ -59,9 +77,51 @@ func TestCmdShowDocument(t *testing.T) {
 				require.Error(t, err)
 			} else {
 				require.NoError(t, err)
-				var resp types.QueryGetDocumentResponse
-				require.NoError(t, ctx.JSONMarshaler.UnmarshalJSON(out.Bytes(), &resp))
-				require.Equal(t, &types.ValidDocument, resp.Document)
+				var response types.QueryGetDocumentResponse
+				require.NoError(t, ctx.JSONMarshaler.UnmarshalJSON(out.Bytes(), &response))
+				require.Equal(t, tt.expected, response.Document)
+			}
+		})
+	}
+}
+
+func testCmdSentDocuments(t *testing.T) {
+
+	for _, tt := range []struct {
+		name     string
+		args     []string
+		expected []*types.Document
+		wantErr  bool
+	}{
+		{
+			name:     "ok",
+			args:     []string{types.ValidDocument.Sender},
+			expected: documentsGenesisState.Documents,
+		},
+		{
+			name:    "invalid address",
+			args:    []string{"abc"},
+			wantErr: true,
+		},
+		{
+			name: "no documents expected",
+			args: []string{types.ValidDocumentReceiptRecipient1.Sender},
+		},
+		{
+			name:    "no args",
+			args:    []string{},
+			wantErr: true,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdSentDocuments(), tt.args)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				var response types.QueryGetSentDocumentsResponse
+				require.NoError(t, ctx.JSONMarshaler.UnmarshalJSON(out.Bytes(), &response))
+				require.ElementsMatch(t, tt.expected, response.Document)
 			}
 		})
 	}
