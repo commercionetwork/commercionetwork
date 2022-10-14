5e3a263c7f7a7fdaa7fc3bd98750c94c757c9d6d
Marco Ruaro <marco.ruaro@tradenet.it>
2022-10-14 19:32:00 +0200

    Remove try

--- cmd/commercionetworkd/cmd/root.go
+++ cmd/commercionetworkd/cmd/root.go
@@ -245,18 +245,13 @@ func (a appCreator) newApp(logger log.Logger, db dbm.DB, traceStore io.Writer, a
 		wasmOpts = append(wasmOpts, wasmkeeper.WithVMCacheMetrics(prometheus.DefaultRegisterer))
 	}
 
-	homePath := cast.ToString(appOpts.Get(flags.FlagHome).(string))
-	if homePath == "" {
-		homePath = app.DefaultNodeHome
-	}
-
 	return app.New(
 		logger,
 		db,
 		traceStore,
 		true,
 		skipUpgradeHeights,
-		homePath,
+		cast.ToString(appOpts.Get(flags.FlagHome).(string)),
 		cast.ToUint(appOpts.Get(server.FlagInvCheckPeriod)),
 		a.encCfg,
 		appOpts,
